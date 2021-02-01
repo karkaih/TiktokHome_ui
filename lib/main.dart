c4ffd8f6258f67af353373805b5038999d61f5dd
karkaih <achrafkarkaih@gmail.com>
2021-02-01 21:35:23 +0100

    Tiktok Home

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_ui/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
+}
